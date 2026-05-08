b22dae1e0f0caa0790869990f58b333415ad91a5
ibharatsaini <sainib601@gmail.com>
2026-05-08 17:08:52 +0530

    added node registry class

---
+++ src/node_types/NodeTypes.ts
@@ -0,0 +1,40 @@
+
+import { INodeType } from '../lib/types';
+import { WebhookNode }  from './WebhookNode';
+import { ScheduleNode } from './ScheduleNode';
+import { IfNode }       from './IfNode';
+import { TelegramNode } from './TelegramNode';
+import { SlackNode }    from './SlackNode';
+
+export class NodeTypes {
+  private readonly registry = new Map<string, INodeType>();
+
+  constructor(
+    webhookNode:  WebhookNode,
+    scheduleNode: ScheduleNode,
+    ifNode:       IfNode,
+    telegramNode: TelegramNode,
+    slackNode:    SlackNode,
+  ) {
+    this.register(webhookNode);
+    this.register(scheduleNode);
+    this.register(ifNode);
+    this.register(telegramNode);
+    this.register(slackNode);
+  }
+
+  private register(node: INodeType): void {
+    this.registry.set(node.description.name, node);
+  }
+
+  /** Throws error if the type is not registered  */
+  getByName(type: string): INodeType {
+    const node = this.registry.get(type);
+    if (!node) throw new Error(`Unknown node type: "${type}"`);
+    return node;
+  }
+
+  getAll(): INodeType[] {
+    return Array.from(this.registry.values());
+  }
+}
